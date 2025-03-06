80385e69eba34bae136d103b61083046efff5dc2
Kevin Dew <kevindew@me.com>
2025-03-06 11:23:44 +0000

    More neovim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,4 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+-- editing a python file is very slow (~2s) without this to stop searching for python3 executable
+vim.g.python3_host_prog = "~/.pyenv/shims/python3"
--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,10 +2,13 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- Disable autoformat for ruby / erb files as it gets things wrong and is annoying
+-- Disable autoformat by default - it's annoying for anything where we don't lint a formatter
+vim.g.autoformat = false
+
+-- Enable autoformat for specific file types
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "ruby", "eruby" },
+  pattern = { "python" },
   callback = function()
-    vim.b.autoformat = false
+    vim.b.autoformat = true
   end,
 })
