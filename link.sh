04306d04fbdb0f8c208fb924e4d9f16a363e000d
Kevin Dew <kevindew@me.com>
2025-04-07 10:54:02 +0100

    Use .config/git/ignore as place for global gitignore
    To embrace common conventions.
    
    Also removes the unconfigured .git_template directory

--- link.sh
+++ link.sh
@@ -8,7 +8,6 @@ ln -s ~/.zshrc ~/.zprofile
 ln -s "${SCRIPTDIR}/bash_aliases" ~/.bash_aliases
 ln -s "${SCRIPTDIR}/vimrc" ~/.vimrc
 ln -s "${SCRIPTDIR}/pryrc" ~/.pryrc
-ln -s "${SCRIPTDIR}/gitignore_global" ~/.gitignore_global
 ln -s "${SCRIPTDIR}/ssh-config" ~/.ssh/config
 ln -s "${SCRIPTDIR}/gitconfig" ~/.gitconfig
 ln -s "${SCRIPTDIR}/Brewfile" ~/Brewfile
@@ -17,4 +16,8 @@ if [ ! -d ~/.config ]; then
   mkdir "~/.config"
 fi
 ln -s "${SCRIPTDIR}/nvim" ~/.config/nvim
+if [ ! -d ~/.config/git ]; then
+  mkdir "~/.config/git"
+fi
+ln -s "${SCRIPTDIR}/gitignore_global" ~/.config/git/ignore
 ln -s "${SCRIPTDIR}/psqlrc" ~/.psqlrc
